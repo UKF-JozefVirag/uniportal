4fb37e6852bbda4154808271fcfdb3453cc6dae7
JV <jozef.virag10@gmail.com>
2023-10-11 17:13:22 +0200

    initial commit + sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+  import SideBar from './components/SideBar.vue';
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <v-app>
+    <SideBar />
+    <v-main>
+      <!--CONTENT-->
+    </v-main>
+  </v-app>
+</template>
+
+
---
+++ src/components/SideBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+            title="John Doe"
+            subtitle="jdoe22@gmail.com"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="Projekty" value="projekty"></v-list-item>
+          <v-list-item prepend-icon="mdi-file-document-multiple-outline" title="Publikácie" value="publikacie"></v-list-item>
+          <v-list-item prepend-icon="mdi-chart-bar" title="Štatistiky" value="statistiky"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+  </template>
